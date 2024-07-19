58b9ebf2b5b494d0dc4569cb08092ad60b9b2bd7
Marko Simundza <markosimundza@gmail.com>
2024-07-19 14:53:27 +0200

    advent of code day 6 part 1 DONE

--- src/6_1.js
+++ src/6_1.js
@@ -1,1 +1,36 @@
 // https://adventofcode.com/2023/day/6
+
+import { log } from './util/helpers.js';
+
+let input = `Time:      7  15   30
+Distance:  9  40  200`;
+
+input = input.split('\n').map((line) => {
+  const numberRegex = /\d+/g;
+  return line.match(numberRegex);
+});
+const [time, distance] = input;
+let timeDistancePairs = time.map((time, i) => [
+  Number(time),
+  Number(distance[i]),
+]);
+
+let differentWaysToWin = [];
+
+for (const [time, distance] of timeDistancePairs) {
+  let counter = 0;
+  for (let i = 1; i < time; i++) {
+    let holdTime = i;
+    let travelTime = time - i;
+    let newDistance = holdTime * travelTime;
+
+    if (newDistance > distance) {
+      counter++;
+    }
+  }
+  differentWaysToWin.push(counter);
+}
+
+differentWaysToWin = differentWaysToWin.reduce((acc, curr) => acc * curr);
+
+log(differentWaysToWin);
