18d8af56dda097163de35b284f501da50f359d63
Marko Simundza <markosimundza@gmail.com>
2024-07-24 18:02:51 +0200

    advent of code day 7 part 2 DONE

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,3 +1,12 @@
 export const log = (...args) => {
   console.log(...args);
 };
+
+export const findPropertyWithValue = (obj, value) => {
+  for (const key of Object.keys(obj)) {
+    if (obj[key] === value) {
+      return key; // Return the key of the property that matches the value
+    }
+  }
+  return null; // Return null if no matching property value is found
+};
